a06dc8892bc7724c2133903708f793a24a513903
Scottie Crowe <scottiecrowe@gmail.com>
2022-12-12 12:00:29 -0700

    add event listener to mobile navigation

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const hamburger = document.getElementById("hamburger");
 const closeIcon = document.getElementById("closeIcon");
+const mbNav = document.querySelector(".mb-nav");
+
+hamburger.addEventListener('click', () => {
+    if (hamburger) {
+        mbNav.style.display = "flex"
+    } else {
+        mbNav.style.display = 'none'
+    }
+})
+
+closeIcon.addEventListener('click', () => {
+    if (closeIcon) {
+        mbNav.style.display = "none"
+    } else {
+        mbNav.style.display = "flex"
+    }
+})
