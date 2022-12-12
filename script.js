91a56c30198444fbfdcef57f7f2f3c65989ef541
Scott <scottiecrowe@gmal.com>
2022-12-12 15:39:38 -0700

    refactor menu toggle and menu styles

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const hamburger = document.getElementById("hamburger");
-const closeIcon = document.getElementById("closeIcon");
-const mbNav = document.querySelector(".mb-nav");
+const navIconClose = document.getElementById('nav-icon-close');
+const navIconHamburger = document.getElementById('nav-icon-hamburger')
+const mbNav = document.querySelector('.mb-nav');
 
-hamburger.addEventListener('click', () => {
-    if (hamburger) {
-        mbNav.style.display = "flex"
-    } else {
-        mbNav.style.display = 'none'
-    }
+navIconHamburger.addEventListener('click', () => {
+    mbNav.style.display = "flex";
+    navIconHamburger.style.display = "none";
 })
 
-closeIcon.addEventListener('click', () => {
-    if (closeIcon) {
-        mbNav.style.display = "none"
-    } else {
-        mbNav.style.display = "flex"
-    }
+navIconClose.addEventListener('click', () => {
+    mbNav.style.display = "none";
+    navIconHamburger.style.display = "flex";
 })
