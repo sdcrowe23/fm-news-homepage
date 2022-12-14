efb6816b7b911d8ff39d131847d0bf7ba8b0141e
Scott <scottiecrowe@gmal.com>
2022-12-13 23:50:56 -0700

    fix navigation issues

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const navIconClose = document.getElementById('nav-icon-close');
-const navIconHamburger = document.getElementById('nav-icon-hamburger')
+const navIconHamburger = document.getElementById('nav-icon-hamburger');
+const mbNavContainer = document.querySelector('.navigation-container');
+const mbNavOverlay = document.querySelector('.overlay');
 const mbNav = document.querySelector('.mb-nav');
 
 navIconHamburger.addEventListener('click', () => {
-    mbNav.style.display = "flex";
-    navIconHamburger.style.display = "none";
+    mbNav.style.width = "100%"
+    navIconClose.style.display = "flex"
+    mbNavContainer.style.width = "68.2%";
+    mbNavOverlay.style.width = "100%";
+    mbNavOverlay.style.left = "0";
+    navIconHamburger.style.width = "none";
 })
 
 navIconClose.addEventListener('click', () => {
-    mbNav.style.display = "none";
-    navIconHamburger.style.display = "flex";
+    mbNav.style.width = "0";
+    navIconClose.style.display = "none"
+    mbNavContainer.style.width = "0";
+    mbNavOverlay.style.width = "0";
 })
