55b5fbe1375a6f7aeabffdee55968f231b33be14
Scott <scottiecrowe@gmal.com>
2022-12-14 00:22:42 -0700

    fix navigation icon to move off screen

--- script.js
+++ script.js
@@ -6,16 +6,13 @@ const mbNav = document.querySelector('.mb-nav');
 
 navIconHamburger.addEventListener('click', () => {
     mbNav.style.width = "100%"
-    navIconClose.style.display = "flex"
+    // what does this do?
     mbNavContainer.style.width = "68.2%";
+    // What does this do
     mbNavOverlay.style.width = "100%";
-    mbNavOverlay.style.left = "0";
-    navIconHamburger.style.width = "none";
 })
 
 navIconClose.addEventListener('click', () => {
-    mbNav.style.width = "0";
-    navIconClose.style.display = "none"
     mbNavContainer.style.width = "0";
     mbNavOverlay.style.width = "0";
 })
