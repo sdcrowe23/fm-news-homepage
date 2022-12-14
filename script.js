ae146567d70ac0c2f6221146e6dec1c70b061d31
ewilliams-zoot <ezekiel.williams@zootweb.com>
2022-12-14 11:24:28 -0700

    New PR for your finished ideas

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const navIconClose = document.getElementById('nav-icon-close');
-const navIconHamburger = document.getElementById('nav-icon-hamburger');
-const mbNavContainer = document.querySelector('.navigation-container');
-const mbNavOverlay = document.querySelector('.overlay');
-const mbNav = document.querySelector('.mb-nav');
+((doc) => {
+    const navIconClose = doc.getElementById('nav-icon-close');
+    const navIconHamburger = doc.getElementById('nav-icon-hamburger');
+    const mbNavContainer = doc.querySelector('.navigation-container');
+    const mbNavOverlay = doc.querySelector('.overlay');
 
-navIconHamburger.addEventListener('click', () => {
-    mbNav.style.width = "100%"
-    // what does this do?
-    mbNavContainer.style.width = "68.2%";
-    // What does this do
-    mbNavOverlay.style.width = "100%";
-})
+    navIconHamburger.addEventListener('click', () => {
+        mbNavContainer.style.width = "68.2%";
+        // What does this do
+        mbNavOverlay.style.width = "100%";
+    });
 
-navIconClose.addEventListener('click', () => {
-    mbNavContainer.style.width = "0";
-    mbNavOverlay.style.width = "0";
-})
+    navIconClose.addEventListener('click', () => {
+        mbNavContainer.style.width = "0";
+        mbNavOverlay.style.width = "0";
+    });
+})(document);
